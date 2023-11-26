6794489334205e61562b31b94fbb6f43d679f9fa
Blazer <101361868+lordayomide9@users.noreply.github.com>
2023-11-26 11:17:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let shareCount = 0;
+
+    function shareToGroups() {
+      shareCount++;
+      document.getElementById("shareCounter").innerText = `Shares: ${shareCount}`;
+      document.getElementById("submitButton").disabled = false;
+    }
+    
+   function shareToGroups() {
+      shareCount++;
+      document.getElementById("shareCounter").innerText = `Shares: ${shareCount}`;
+      document.getElementById("submitButton").disabled = false;
+    }
+
+    function submitForm(event) {
+      event.preventDefault(); // Prevents the default form submission
+      alert("Congratulations! You are on the whitelist. You will be credited before 20th of December. 🎊 Share to more People");
+      // You can add additional logic here for submitting data to a server if needed.
+    }
+    
+        let shareCount = 0;
+
+    function shareToGroups() {
+      shareCount++;
+      document.getElementById("shareCounter").innerText = `Shares: ${shareCount}`;
+      document.getElementById("submitButton").disabled = false;
+    }
+
+    function submitForm(event) {
+      event.preventDefault(); // Prevents the default form submission
+      alert("Congratulations! You are on the whitelist. You will be credited before 20th of December. 🎊");
+      // You can add additional logic here for submitting data to a server if needed.
+    }
